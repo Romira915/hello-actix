7f5b45801e2654bfb40432af539a288cfbf44dd5
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-02 11:37:08 +0900

    Add Popular lib

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,15 @@
+use std::{
+    fs::{self, File},
+    path::Path,
+};
+
 use actix_cors::Cors;
 use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+use chrono::{FixedOffset, Utc};
+use log::LevelFilter;
+use simplelog::{
+    ColorChoice, CombinedLogger, ConfigBuilder, SharedLogger, TermLogger, TerminalMode, WriteLogger,
+};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -15,8 +25,38 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+fn init_logger<P>(log_path: Option<P>)
+where
+    P: AsRef<Path>,
+{
+    let jst_now = {
+        let jst = Utc::now();
+        jst.with_timezone(&FixedOffset::east(9 * 3600))
+    };
+    let mut logger: Vec<Box<dyn SharedLogger>> = vec![
+        #[cfg(not(feature = "termcolor"))]
+        TermLogger::new(
+            LevelFilter::Info,
+            ConfigBuilder::new().set_time_to_local(true).build(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+    ];
+    if let Some(log_path) = log_path {
+        fs::create_dir_all(&log_path).unwrap();
+        logger.push(WriteLogger::new(
+            LevelFilter::Warn,
+            ConfigBuilder::new().set_time_to_local(true).build(),
+            File::create(log_path.as_ref().join(format!("{}.log", jst_now))).unwrap(),
+        ));
+    }
+    CombinedLogger::init(logger).unwrap()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    init_logger(Some(&Path::new("./log")));
+
     HttpServer::new(|| {
         App::new()
             .wrap(
---
+++ backend/src/server.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+
+use actix::Recipient;
+
+use crate::message::ChatMessage;
+
+type Client = Recipient<ChatMessage>;
+type Room = HashMap<usize, Client>;
+
+pub struct WsQuizServer {
+    rooms: HashMap<String, Room>,
+}
+
+impl WsQuizServer {
+    fn take_room(&mut self, room_name: &str) -> Option<Room> {
+        let room = self.rooms.get_mut(room_name)?;
+        let room = std::mem::take(room);
+        Some(room)
+    }
+
+    fn add_client_to_room(&mut self, room_name: &str, id: Option<usize>, client: Client) -> usize {
+        let mut id = id.unwrap_or_else(rand::random::<usize>);
+
+        if let Some(room) = self.rooms.get_mut(room_name) {
+            loop {
+                if room.contains_key(&id) {
+                    id = rand::random::<usize>();
+                } else {
+                    break;
+                }
+            }
+
+            room.insert(id, client);
+            return id;
+        }
+
+        // Create a new room for the first client
+        let mut room: Room = HashMap::new();
+
+        room.insert(id, client);
+        self.rooms.insert(room_name.to_owned(), room);
+
+        id
+    }
+
+    fn send_chat_message(&mut self, room_name: &str, msg: &str, _src: usize) -> Option<()> {
+        let mut room = self.take_room(room_name)?;
+
+        for (id, client) in room.drain() {
+            if client.do_send(ChatMessage(msg.to_owned())).is_ok() {
+                self.add_client_to_room(room_name, Some(id), client);
+            }
+        }
+
+        Some(())
+    }
+}
